ac22e0a6a8dd948a3c588b58985ab509a1d1586e
Andrea055 <vivanco11.ac@gmail.com>
2023-02-25 00:19:11 +0000

    add services and make ready production

--- src/routes/servizi/+page.svelte
+++ src/routes/servizi/+page.svelte
@@ -24,5 +24,37 @@
 				image="https://static.vecteezy.com/system/resources/previews/002/136/381/original/mobile-application-development-free-vector.jpg"
 			/>
 		</div>
+		<div>
+			<Servizio
+				titolo="Upgrade sito già esistente"
+				descrizione="Hai un sito già esistente ma è vecchio e lento? 
+				Noi possiamo rinnovarlo offrendo una nuova grafica e velocizzarlo grazie all'uso di tecnologie all'avanguardia
+				come Svelte e altri framework."
+				image="https://img.freepik.com/free-vector/space-rocket-flying-space-with-moon-stars-background-print-vector-illustration_1284-2184.jpg?w=2000"
+			/>
+		</div>
+		<div>
+			<Servizio
+				titolo="Creazione QR code"
+				descrizione="Devi condividere un link con i tuoi clienti in modo rapido? Usa il codice QR.
+				MySites può aiutarti! Possiamo creare codici QR per tutte le tue esigenze."
+				image="http://niceillustrations.com/wp-content/uploads/2020/09/QR-Code.png"
+			/>
+		</div>
+		<div>
+			<Servizio
+				titolo="Installazione SO e preparazione computer"
+				descrizione="La tua azienda deve configurare dei computer? Ci pensiamo noi!"
+				image="https://img.freepik.com/premium-vector/system-software-update-illustration-design-concept-illustration-websites-landing-pages-mobile-applications-posters-banners_108061-821.jpg?w=2000"
+			/>
+		</div>
+		<div>
+			<Servizio
+				titolo="Creazione post, storie o reel"
+				descrizione="Vuoi essere più presente sui social? MySites può aiutarti!
+				Possiamo gestire il tuo account Instagram o TikTok e pubblicare contenuti per aumentare l'interazione con il tuo pubblico social."
+				image="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/eb7147103984155.5f58fb3d5bbeb.jpg"
+			/>
+		</div>
 	</div>
 </center>
