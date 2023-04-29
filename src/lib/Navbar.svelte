9a33b3f8aafb0f371ca6ed6f9d744b3398b7e4d9
Andrea055 <vivanco11.ac@gmail.com>
2023-04-29 19:01:24 +0200

    refactor navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,38 +1,14 @@
-<script>
-	let y;
-	console.log(y);
-</script>
-
-{#if y < 290}
-	<!-- navbar -->
-	<nav class="flex justify-between text-white sticky top-0 nav_color">
-		<div class="px-5 xl:px-12 py-6 flex w-full items-center">
-			<a class="text-3xl font-bold font-heading" href="/">
-				<img class="h-9 logo_size" src="favicon_no_bg.png" alt="logo" />
-			</a>
-			<!-- Nav Links -->
-			<ul class="hidden md:flex px-4 mx-8 space-x-12 text-xl font_spesciale ">
-				<li><a class="hover:text-gray-200" href="/">Home</a></li>
-				<li><a class="hover:text-gray-200" href="/servizi">Servizi</a></li>
-			</ul>
-		</div>
-	</nav>
-	{:else}
-		<!-- navbar -->
-		<nav class="flex justify-between text-white sticky top-0 nav_color_white">
-			<div class="px-5 xl:px-12 py-6 flex w-full items-center">
-				<a class="text-3xl font-bold font-heading" href="/">
-					<img class="h-9 logo_size" src="favicon_no_bg.png" alt="logo" />
-				</a>
-				<!-- Nav Links -->
-				<ul class="hidden md:flex px-4 mx-8 space-x-12 text-xl font_spesciale ">
-					<li><a class="hover:text-gray-200" href="/">Home</a></li>
-					<li><a class="hover:text-gray-200" href="/servizi">Servizi</a></li>
-				</ul>
-			</div>
-		</nav>
-{/if}
-<svelte:window bind:scrollY={y} />
+<nav class="flex justify-between text-white sticky top-0">
+	<div class="px-5 xl:px-12 py-6 flex w-full items-center">
+		<a href="/" class="flex items-center">
+			<img src="/favicon_no_bg.png" class="h-9 logo_size" alt="Flowbite Logo" />
+		</a>
+		<ul class="flex mx-8 text-xl font_bello space-x-12">
+			<li><a class="hover:text-gray-200" href="/">Home</a></li>
+			<li><a class="hover:text-gray-200" href="/servizi">Servizi</a></li>
+		</ul>
+	</div>
+</nav>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap');
@@ -40,14 +16,7 @@
 		width: 100px;
 		height: 70px;
 	}
-	.nav_color {
-		background-color: #ffffff00;
-	}
-
-	.nav_color_white {
-		background-color: rgba(96, 94, 94, 0.588);
-	}
-	.font_spesciale {
+	.font_bello {
 		font-family: 'Inter', sans-serif;
 	}
 </style>
