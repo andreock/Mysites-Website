c5c0ed68167e81911de462901db0e169acd87b78
Andrea055 <vivanco11.ac@gmail.com>
2023-05-01 17:47:45 +0200

    refactor servizi

--- src/lib/Servizio.svelte
+++ src/lib/Servizio.svelte
@@ -12,13 +12,14 @@
 	});
 </script>
 
-<div class="card" style="background-color: {color}">
-	<h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{titolo}</h4>
-	<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{descrizione}</p>
-	<a href={link}>Scopri di più <span class="animate__animated animate__fadeIn">&#8594;</span></a>
-	<div class="flex justify-center">
-		<img src={image} alt="" class="rounded-lg;"/>
+<div class="card">
+	<div class="flex justify-start pb-2">
+		<img src={image} alt="" class="rounded-lg h-28 w-26" />
 	</div>
+	<h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{titolo}</h4>
+
+	<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 ">{descrizione}</p>
+	<a href={link}>Scopri di più <span class="animate__animated animate__fadeIn">&#8594;</span></a>
 </div>
 
 <style>
@@ -26,18 +27,25 @@
 		position: relative;
 		border-radius: 30px;
 		background-color: rgb(255, 255, 255);
-		width: 20em;
-		height: 35em;
+		width: 30em;
+		height: 22em;
 		padding: 3em;
 		box-shadow: 2px 2px 5px #2b52ff75;
+		text-align: left;
+		background: linear-gradient(to right, rgba(16, 99, 251, 0.5) 50%, white 50%);
+		background-size: 200% 100%;
+		background-position: right top;
+		transition: all 0.5s ease-out;
+	}
+	.card:hover {
+		background-position: left top;
 	}
 
-	.card > div > img {
-		position: absolute;
-		bottom: 0;
-		width: 22em;
-		margin-bottom: 1em;
-		padding: 2em;
+	@media (max-width: 600px) {
+		.card {
+			width: 26em;
+			height: 22em;
+			padding: 3em;
+		}
 	}
-	
 </style>
--- src/routes/servizi/+page.svelte
+++ src/routes/servizi/+page.svelte
@@ -2,79 +2,82 @@
 	import Servizio from '$lib/Servizio.svelte';
 </script>
 
+<h1 class="title" id="servizi">I nostri servizi</h1>
+<br />
+<br />
+<br />
+<!--TODO: Remove this center -->
 <center>
-	<h1 class="title" id="servizi">I nostri servizi</h1>
-	<br />
-	<br />
-	<br />
-	<div class="grid xl:grid-cols-4 sm:grid-cols-2 md:grid-cols-2 gap-4">
-		<div>
-			<Servizio
-				titolo="Creazione siti web"
-				descrizione="Creiamo siti web responsive, con i più moderni CMS, veloci e adatti a tutte le piattaforme"
-				image="https://media.istockphoto.com/id/1277185062/vector/christmas-online-gifts-mobile-app-in-phone-for-xmas-holiday-smartphone-in-hand-with-online.jpg?s=612x612&w=0&k=20&c=HcJNemga63FQWYLQnDvmGXEzGWWtN_fq5vR5w-ugZco="
-				link="/siti"
-				color="rgba(16, 99, 251, 0.4)"
-			/>
-		</div>
-		<div>
-			<Servizio
-				titolo="Creazione app mobili"
-				descrizione="Creiamo applicazioni per tutte le piattaforme, scritte con i migliori framework tra qui Flutter e Nativescript."
-				image="https://static.vecteezy.com/system/resources/previews/002/136/381/original/mobile-application-development-free-vector.jpg"
-				link="/app"
-			/>
-		</div>
-		<div>
-			<Servizio
-				titolo="Upgrade sito già esistente"
-				descrizione="Hai un sito già esistente ma è vecchio e lento? 
-				Noi possiamo rinnovarlo offrendo una nuova grafica e velocizzarlo."
-				image="https://img.freepik.com/free-vector/space-rocket-flying-space-with-moon-stars-background-print-vector-illustration_1284-2184.jpg?w=2000"
-				link="/upgrade"
-				color="rgba(16, 99, 251, 0.4)"
-			/>
-		</div>
-		<div>
-			<Servizio
-				titolo="Installazione SO e preparazione computer"
-				descrizione="La tua azienda deve configurare dei computer? Ci pensiamo noi!"
-				image="https://img.freepik.com/premium-vector/system-software-update-illustration-design-concept-illustration-websites-landing-pages-mobile-applications-posters-banners_108061-821.jpg?w=2000"
-				link="/install_so"
-			/>
-		</div>
-		<div>
-			<Servizio
-				titolo="Creazione post, storie o reel"
-				descrizione="Vuoi essere più presente sui social? MySites può aiutarti!
-				Possiamo gestire il tuo account Instagram o TikTok."
-				image="https://img.freepik.com/free-vector/social-media-concept-illustration_114360-2525.jpg?w=2000"
-				link="/post"
-				color="rgba(16, 99, 251, 0.4)"
-			/>
-		</div>
-
-		<div>
-			<Servizio
-				titolo="Server Minecraft"
-				descrizione="Hai bisogno di un server Minecraft? Offriamo server MC controllabili via interfaccia web e anti-DDoS."
-				image="https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP"
-				link="/mc"
-			/>
-		</div>
-
-		<div>
-			<Servizio
-				titolo="Creazione bot Telegram e Discord"
-				descrizione="Hai bisogno di un bot per Discord o Telegram? Mysites fa per te! 
-				Creaiamo bot Telegram e Discord per tutte le tue esigenze."
-				image="https://media.istockphoto.com/id/1145772045/vector/white-paper-plane-on-blue-background-vector-illustration-telegram-icon.jpg?s=170667a&w=0&k=20&c=ye8nZBo13rGpgmxVqbLn-UPKZCU_J_W2FNW7q70uqvY="
-				link="/bot"
-				color="rgba(16, 99, 251, 0.4)"
-			/>
-		</div>
+<div class="grid xl:grid-cols-3 sm:grid-cols-2 md:grid-cols-2 gap-4">
+	<div>
+		<Servizio
+			titolo="Creazione siti web"
+			descrizione="Creiamo siti web responsive, con i più moderni CMS, veloci e adatti a tutte le piattaforme"
+			image="/sito.png"
+			link="/siti"
+			color="rgba(16, 99, 251, 0.4)"
+		/>
 	</div>
+	<div>
+		<Servizio
+			titolo="Creazione app mobili"
+			descrizione="Creiamo applicazioni per tutte le piattaforme, scritte con i migliori framework tra qui Flutter e Nativescript."
+			image="/app.png"
+			link="/app"
+		/>
+	</div>
+	<div>
+		<Servizio
+			titolo="Upgrade sito già esistente"
+			descrizione="Hai un sito già esistente ma è vecchio e lento? 
+					Noi possiamo rinnovarlo offrendo una nuova grafica e velocizzarlo."
+			image="/upgrade.png"
+			link="/upgrade"
+			color="rgba(16, 99, 251, 0.4)"
+		/>
+	</div>
+	<div>
+		<Servizio
+			titolo="Installazione SO e preparazione computer"
+			descrizione="La tua azienda deve configurare dei computer? Ci pensiamo noi!"
+			image="/manutenzione.png"
+			link="/install_so"
+		/>
+	</div>
+	<div>
+		<Servizio
+			titolo="Creazione post, storie o reel"
+			descrizione="Vuoi essere più presente sui social? MySites può aiutarti!
+					Possiamo gestire il tuo account Instagram o TikTok."
+			image="/social.png"
+			link="/post"
+			color="rgba(16, 99, 251, 0.4)"
+		/>
+	</div>
+
+	<div>
+		<Servizio
+			titolo="Server Minecraft"
+			descrizione="Hai bisogno di un server Minecraft? Offriamo server MC controllabili via interfaccia web e anti-DDoS."
+			image="/minecraft.png"
+			link="/mc"
+		/>
+	</div>
+
+	<div>
+		<Servizio
+			titolo="Creazione bot Telegram e Discord"
+			descrizione="Hai bisogno di un bot per Discord o Telegram? Mysites fa per te! 
+					Creaiamo bot Telegram e Discord per tutte le tue esigenze."
+			image="/tg.png"
+			link="/bot"
+			color="rgba(16, 99, 251, 0.4)"
+		/>
+	</div>
+</div>
 </center>
+
+<br>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@1,500&display=swap');
