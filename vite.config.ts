8d39e27b57fe4d84a6c3ca4f6efe3ea80b85d89f
Andrea055 <vivanco11.ac@gmail.com>
2023-05-27 12:57:31 +0200

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { Sitemap } from 'vite-plugin-svelte-sitemap';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), Sitemap({
+		baseurl: "https://mysites-service.com"
+	})],
 	optimizeDeps: {
 		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
 	}
